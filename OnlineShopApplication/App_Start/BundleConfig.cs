c8b65fa76137692b6aa1044fe1048c30ab7e3006
BartoszJaskolski <bart.jaskolski@wp.pl>
2018-05-28 21:08:20 +0200

    "Framewors Angular / BundleConfig "

--- OnlineShopApplication/App_Start/BundleConfig.cs
+++ OnlineShopApplication/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace OnlineShopApplication
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "Scripts/angular.js",
+                        "Scripts/angular-route.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
