eb04c7b5e6d5177dd20e53042bbd6ec8b57af2fd
BartoszJaskolski <bart.jaskolski@wp.pl>
2018-06-07 11:20:40 +0200

    AutoComplete

--- OnlineShopApplication/Controllers/ProductController.cs
+++ OnlineShopApplication/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace OnlineShopApplication.Controllers
 {
@@ -31,6 +32,26 @@ namespace OnlineShopApplication.Controllers
                 product = db.Products.Where(a => a.CategoryID.Equals(categoryID)).OrderBy(a => a.ProductName).ToList();
             }
             return new JsonResult { Data = product, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        //Find products by name
+        public JsonResult SearchProduct(string searchText)
+        {
+            List<Product> products = new List<Product>();
+            using(ShopAppDatabaseEntities db = new ShopAppDatabaseEntities())
+            {
+                products = db.Products.Where(a => a.ProductName.StartsWith(searchText)).OrderBy(a => a.ProductName).ToList();
+            }
+            return new JsonResult { Data = products, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        //Fetch only all products names using in autocoplete 
+        public JsonResult GetProductNamesList()
+        {
+
+            using(ShopAppDatabaseEntities db = new ShopAppDatabaseEntities())
+            {
+                var productNames = db.Products.Select(x => new { x.ProductName }).ToList();
+                return new JsonResult { Data = productNames, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
         }
 
     }
--- OnlineShopApplication/Scripts/ProductController/PSearchAngullarController.js
+++ OnlineShopApplication/Scripts/ProductController/PSearchAngullarController.js
@@ -3,18 +3,43 @@
     $scope.message = "Works";
     $scope.SearchString = "SearchString"
 
-    $scope.SearchProduct = function (search, category) {
-        PSearchService.SearchP()
+    $scope.list = ["asdasd", "bartek", "Liana", "Jacek", "irek"];
+    PSearchService.SearchP().then(function (d) {
+        $scope.ProductList = d.data.Data;
+    }, function (error) {
+        alert('error !');
+    });
 
+    $scope.complete = function (string) {
+        var output = [];
+
+        angular.forEach($scope.ProductList, function (searchText) {
+            if (searchText.ProductName.toLowerCase().indexOf(string.toLowerCase()) >= 0) {
+                output.push(searchText.ProductName);
+            }
+        });
+        $scope.filterProduct = output;
     }
 
-   
+    $scope.fillTextbox = function(string){
+        $scope.searchText = string;
+        $scope.hidethis = true;
+    }
+
+    $scope.SearchProduct = function (search, category) {
+        PSearchService.SearchP()
+    }
 })
 .factory('PSearchService', function($http){
-    var factory = {}
+    var factory = {};
 
-    factory.SearchP = function () { };
+    factory.SearchP = function () {
+        return $http.get('/api/Product/GetProductNamesList');
+    };
+    
+    factory.SearchProduct = function () {
+        return $http.get('/api/Product/SearchProduct')
+    }
 
     return factory;
-
 })
